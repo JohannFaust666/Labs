32613809dd61fbd41ffc46d61741ebb460a5bdb0
Alexander <123825756+JohannFaust666@users.noreply.github.com>
2023-05-15 22:30:40 +0700

    Create mainForLib.c

---
+++ Module5/Lab4/libs/mainForLib.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "arithmetics.h"
+
+int main() {
+    int a = 5;
+    int b = 3;
+
+    printf("%d + %d = %d\n", a, b, add(a, b));
+    printf("%d - %d = %d\n", a, b, subtract(a, b));
+
+    return 0;
+}
